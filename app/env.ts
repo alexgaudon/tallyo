93a2b4bfa692a22935f4d9a463057c689b051d45
Alex Gaudon <agaudon@proton.me>
2025-02-08 20:56:47 -0330

    fix env.ts

--- app/env.ts
+++ app/env.ts
@@ -3,10 +3,15 @@ import "dotenv/config";
 import { z } from "zod";
 
 const envSchema = z.object({
-  DATABASE_URL: z.string(),
+  DATABASE_URL: z.string().default("file:local.db"),
   DATABASE_TOKEN: z.string().optional(),
-  VITE_APP_NAME: z.string(),
-  VITE_APP_TITLE: z.string(),
+  VITE_APP_NAME: z.string().default("Tallyo"),
+  VITE_APP_TITLE: z.string().default("Personal Finance"),
+  BETTER_AUTH_SECRET: z.string(),
+  BETTER_AUTH_URL: z.string(),
+  DISCORD_CLIENT_ID: z.string(),
+  DISCORD_CLIENT_SECRET: z.string(),
+  DISCORD_REDIRECT_URL: z.string(),
 });
 
 export const safeEnv = envSchema.parse(process.env);
