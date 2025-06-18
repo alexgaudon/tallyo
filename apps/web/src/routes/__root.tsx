04084aac76b91e0d497ba9d8c9942992ce78aaef
ag <agaudon@proton.me>
2025-06-17 23:02:26 -0230

    added back button on signin page

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -10,11 +10,13 @@ import type { QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import {
 	HeadContent,
+	Link,
 	Outlet,
 	createRootRouteWithContext,
 	useLocation,
 } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { ArrowLeft } from "lucide-react";
 import { useState } from "react";
 import type { appRouter } from "../../../server/src/routers";
 import "../index.css";
@@ -69,7 +71,15 @@ function RootComponent() {
 			<ORPCContext.Provider value={orpcUtils}>
 				<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
 					<div className="grid h-svh grid-rows-[auto_1fr]">
-						{location.pathname.startsWith("/sign") ? null : <Header />}
+						{location.pathname.startsWith("/sign") ? (
+							<div className="h-16 mt-8 ml-8">
+								<Link to="/">
+									<ArrowLeft className="h-4 w-4" />
+								</Link>
+							</div>
+						) : (
+							<Header />
+						)}
 						<Outlet />
 					</div>
 					<Toaster richColors />
